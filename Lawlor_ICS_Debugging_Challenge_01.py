68174661907194052026d525647d892895ab2e48
Priyanshu <59302986+Priyanshu-CODERX@users.noreply.github.com>
2020-10-01 10:39:33 +0530

    Fixed the syntax errors
    I fixed all possible syntax errors and made the code working

--- Lawlor_ICS_Debugging_Challenge_01.py
+++ Lawlor_ICS_Debugging_Challenge_01.py
@@ -5,17 +5,15 @@
 # Code pulled from the short programs I used to solve the Python Challenge problems
 
 # Ideally, the program should print the words "cat" and "dog"
-# SYNTAX ERROR COUNT: 4
+# SYNTAX ERROR COUNT: 4 : Solved
 
 text = "a_r"
 # empty string variable that can be added to later
 message = ''
 for x in text:
-    # figure out the corresponding ASCII value of each character and add 2 to it
+	# figure out the corresponding ASCII value of each character and add 2 to it
     # convert the new ASCII values back into characters and assemble those characters into a new message
-    message = message + chr((ord(x) + 2)
-
-# print the new message
+	message = message + chr(ord(x) + 2)
 print(message)
 
 # another empty string variable that can be added to later
@@ -26,10 +24,9 @@ for x in fhand:
     line = str(x)
     for y in line:
         # if python finds any letters hidden among the various symbols found in the text document, assemble them into a new message
-        if y = 'a' or y == 'b' or y == 'c' or y == 'd' or y == 'e' or y == 'f' or y == 'g' or y == 'h' or y == 'i' or y == 'j'
-                or y == 'k' or y == 'l' or y == 'm' or y == 'n' or y == 'o' or y == 'p' or y == 'q' or y == 'r' or y == 's' or
-                y == 't' or y == 'u' or y == 'v' or y == 'w' or y == 'x' or y == 'y' or y == 'z':
-            message2 = message2 + y
+        if y == 'a' or y == 'b' or y == 'c'or y == 'd'or y == 'e'or y == 'f'or y == 'g'or y == 'h'or y == 'i'or y == 'j'or y == 'k'or y == 'l'or y == 'm' or y == 'n'or y == 'o'or y == 'p'or y == 'q'or y == 'r'or y == 's'or y == 't'or y == 'u'or y == 'v'or y == 'w'or y == 'x'or y == 'y'or y == 'z':
+        	message2 += y
+            # message2 = message2 + y
 
 # print the second new message
 print(message2)
